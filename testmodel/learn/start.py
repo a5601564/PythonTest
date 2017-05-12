ec6c73c3faf0e1ba049cd596df962e686de29aaf
李品良 <474857849@qq.com>
2017-05-12 16:12:33 +0800

    222

--- testmodel/learn/start.py
+++ testmodel/learn/start.py
@@ -1,32 +1,50 @@
 import tensorflow as tf
-import numpy as np
 
-# 使用 NumPy 生成假数据(phony data), 总共 100 个点.
-x_data = np.float32(np.random.rand(2, 100)) # 随机输入
-y_data = np.dot([0.100, 0.200], x_data) + 0.300
 
-# 构造一个线性模型
-# 
-b = tf.Variable(tf.zeros([1]))
-W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
-y = tf.matmul(W, x_data) + b
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    # add one more layer and return the output of this layer
+    # 区别：大框架，定义层 layer，里面有 小部件
+    with tf.name_scope('layer'):
+        # 区别：小部件
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b, )
+        return outputs
 
-# 最小化方差
-loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
 
-# 初始化变量
-init = tf.initialize_all_variables()
+# define placeholder for inputs to network
+# 区别：大框架，里面有 inputs x，y
+with tf.name_scope('inputs'):
+    xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
+    ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
 
-# 启动图 (graph)
+# add hidden layer
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+# add output layer
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+# the error between prediciton and real data
+# 区别：定义框架 loss
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                                        reduction_indices=[1]))
+
+# 区别：定义框架 train
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
 sess = tf.Session()
-sess.run(init)
 
-# 拟合平面
-for step in range(0, 201):
-    sess.run(train)
-    if step % 20 == 0:
-        print (step, sess.run(W), sess.run(b))
-
-        # 得到最佳拟合结果 W: [[0.100  0.200]], b: [0.300]
+# 区别：sess.graph 把所有框架加载到一个文件中放到文件夹"logs/"里
+# 接着打开terminal，进入你存放的文件夹地址上一层，运行命令 tensorboard --logdir='logs/'
+# 会返回一个地址，然后用浏览器打开这个地址，在 graph 标签栏下打开
+writer = tf.summary.FileWriter("logs/", sess.graph)
+# important step
+sess.run(tf.initialize_all_variables())
